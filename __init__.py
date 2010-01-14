e05caa7999f5c66f8fb147fcd581946b98b6bc24
Detlev Offenbach <detlev@die-offenbachs.de>
2010-01-14 18:35:52 +0000

    Corrected the version docu and regenerated the source docu.

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@
 #
 
 """
-Package implementing the eric5 Python IDE (version 4.4).
+Package implementing the eric5 Python IDE (version 5.0).
 
 To get more information about eric5 please see the 
 <a href="http://eric-ide.python-projects.org/index.html">eric web site</a>.
