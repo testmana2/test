a041bb0b32b729a0dba816a37fdf29c883254a73
Detlev Offenbach <detlev@die-offenbachs.de>
2011-05-30 19:24:26 +0200

    Fixed an issue in the Subversion (svn) log browser and an issue in the Subversion (svn) tag/branch list dialog.

--- Plugins/VcsPlugins/vcsSubversion/SvnLogBrowserDialog.py
+++ Plugins/VcsPlugins/vcsSubversion/SvnLogBrowserDialog.py
@@ -154,7 +154,7 @@ class SvnLogBrowserDialog(QDialog, Ui_SvnLogBrowserDialog):
             msg.append(line.strip())
         
         itm = QTreeWidgetItem(self.logTree, [
-            "{0:7}".format(revision), 
+            "{0:>7}".format(revision), 
             author, 
             date, 
             " ".join(msg), 
