c9f668d83df788f27ffe4feec87be63f29b17ed0
Detlev Offenbach <detlev@die-offenbachs.de>
2011-07-04 08:08:04 +0200

    Changed the non-existance of a Python2 interpreter in the syntax checker to a warning. (transplanted from 8beb6bacd874e8032252c4625c4c2c6233176713)

--- Utilities/__init__.py
+++ Utilities/__init__.py
@@ -1151,10 +1151,11 @@ def py2compile(file, checkFlakes = False):
     """
     interpreter = Preferences.getDebugger("PythonInterpreter")
     if interpreter == "" or not isinpath(interpreter):
-        return (True, file, "1", "", 
+        return (False, "", "", "", "", [(
+            "", "1",
             QCoreApplication.translate("Utilities", 
-                                       "Python2 interpreter not configured."), 
-            [])
+                                       "Python2 interpreter not configured.")
+            )])
     
     syntaxChecker = os.path.join(getConfig('ericDir'), 
                                  "UtilitiesPython2", "Py2SyntaxChecker.py")
