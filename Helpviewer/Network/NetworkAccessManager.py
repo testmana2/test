ab66adaf331660cfc85b514dfbc6f84ea1232a90
Detlev Offenbach <detlev@die-offenbachs.de>
2011-08-18 12:36:53 +0200

    Added code to support any SSL protocol.

--- Helpviewer/Network/NetworkAccessManager.py
+++ Helpviewer/Network/NetworkAccessManager.py
@@ -14,7 +14,7 @@ from PyQt4.QtGui import QDialog
 from PyQt4.QtNetwork import QNetworkAccessManager, QNetworkRequest, QNetworkReply
 try:
     from PyQt4.QtNetwork import QSslCertificate, QSslConfiguration, QSslSocket, \
-        QSslError
+        QSslError, QSsl
     SSL_AVAILABLE = True
 except ImportError:
     SSL_AVAILABLE = False
@@ -82,6 +82,7 @@ class NetworkAccessManager(QNetworkAccessManager):
                         caList.append(cert)
             sslCfg = QSslConfiguration.defaultConfiguration()
             sslCfg.setCaCertificates(caList)
+            sslCfg.setProtocol(QSsl.AnyProtocol)
             QSslConfiguration.setDefaultConfiguration(sslCfg)
             
             self.sslErrors.connect(self.__sslErrors)
@@ -252,6 +253,7 @@ class NetworkAccessManager(QNetworkAccessManager):
                     for cert in caNew:
                         caList.append(cert)
                     sslCfg.setCaCertificates(caList)
+                    sslCfg.setProtocol(QSsl.AnyProtocol)
                     QSslConfiguration.setDefaultConfiguration(sslCfg)
                     reply.setSslConfiguration(sslCfg)
                     
