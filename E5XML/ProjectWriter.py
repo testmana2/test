d5b0374fdc24f891f5969c2c8f2bc94e91830a8c
Detlev Offenbach <detlev@die-offenbachs.de>
2011-09-05 19:30:37 +0200

    Changed code to write some data in sorted order into the projects file.

--- E5XML/ProjectWriter.py
+++ E5XML/ProjectWriter.py
@@ -162,7 +162,7 @@ class ProjectWriter(XMLStreamWriterBase):
         
         # do the filetype associations
         self.writeStartElement("FiletypeAssociations")
-        for pattern, filetype in list(self.pdata["FILETYPES"].items()):
+        for pattern, filetype in sorted(self.pdata["FILETYPES"].items()):
             self.writeEmptyElement("FiletypeAssociation")
             self.writeAttribute("pattern", pattern)
             self.writeAttribute("type", filetype)
@@ -171,7 +171,7 @@ class ProjectWriter(XMLStreamWriterBase):
         # do the lexer associations
         if self.pdata["LEXERASSOCS"]:
             self.writeStartElement("LexerAssociations")
-            for pattern, lexer in list(self.pdata["LEXERASSOCS"].items()):
+            for pattern, lexer in sorted(self.pdata["LEXERASSOCS"].items()):
                 self.writeEmptyElement("LexerAssociation")
                 self.writeAttribute("pattern", pattern)
                 self.writeAttribute("lexer", lexer)
