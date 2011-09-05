47a4b565fb7602407c455894e47d0617f083deae
Detlev Offenbach <detlev@die-offenbachs.de>
2011-09-05 19:17:27 +0200

    Corrected a display string.

--- Plugins/VcsPlugins/vcsMercurial/ProjectHelper.py
+++ Plugins/VcsPlugins/vcsMercurial/ProjectHelper.py
@@ -1176,7 +1176,7 @@ class HgProjectHelper(VcsProjectHelper):
         shouldReopen = self.vcs.hgUnbundle(self.project.ppath)
         if shouldReopen:
             res = E5MessageBox.yesNo(self.parent(),
-                self.trUtf8("Pull"),
+                self.trUtf8("Apply changegroups"),
                 self.trUtf8("""The project should be reread. Do this now?"""),
                 yesDefault=True)
             if res:
