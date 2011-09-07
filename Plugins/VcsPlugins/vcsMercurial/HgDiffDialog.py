a8cd883f772a10f5c8f122504234a08ea7b2b834
Detlev Offenbach <detlev@die-offenbachs.de>
2011-09-07 18:08:56 +0200

    Fixed an issue in the various diff dialogs writing the patch (this time properly). (transplanted from 9083f6b7ea3ba0274761eb49e2728e71dab9e54d)

--- Plugins/VcsPlugins/vcsMercurial/HgDiffDialog.py
+++ Plugins/VcsPlugins/vcsMercurial/HgDiffDialog.py
@@ -292,7 +292,7 @@ class HgDiffDialog(QWidget, Ui_HgDiffDialog):
         
         eol = e5App().getObject("Project").getEolString()
         try:
-            f = open(fname, "w", encoding = "utf-8")
+            f = open(fname, "w", encoding = "utf-8", newline = "")
             f.write(eol.join(self.contents.toPlainText().splitlines()))
             f.close()
         except IOError as why:
--- Plugins/VcsPlugins/vcsPySvn/SvnDiffDialog.py
+++ Plugins/VcsPlugins/vcsPySvn/SvnDiffDialog.py
@@ -341,7 +341,7 @@ class SvnDiffDialog(QWidget, SvnDialogMixin, Ui_SvnDiffDialog):
         
         eol = e5App().getObject("Project").getEolString()
         try:
-            f = open(fname, "w", encoding = "utf-8")
+            f = open(fname, "w", encoding = "utf-8", newline = "")
             f.write(eol.join(self.contents.toPlainText().splitlines()))
             f.close()
         except IOError as why:
