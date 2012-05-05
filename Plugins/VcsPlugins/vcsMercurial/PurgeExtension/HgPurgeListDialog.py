661d62b1f6ce6c44ee72656c59f5c6790974d287
Detlev Offenbach <detlev@die-offenbachs.de>
2012-05-05 18:55:21 +0200

    Fixed an issue in the dialog code creation dialog with scanning a Python source file not having the standard extension.

---
+++ Plugins/VcsPlugins/vcsMercurial/PurgeExtension/HgPurgeListDialog.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2011 - 2012 Detlev Offenbach <detlev@die-offenbachs.de>
+#
+
+"""
+Module implementing a dialog to list all files not tracked by Mercurial.
+"""
+
+from PyQt4.QtGui import QDialog
+
+from .Ui_HgPurgeListDialog import Ui_HgPurgeListDialog
+
+
+class HgPurgeListDialog(QDialog, Ui_HgPurgeListDialog):
+    """
+    Class implementing a dialog to list all files not tracked by Mercurial.
+    """
+    def __init__(self, entries, parent=None):
+        """
+        Constructor
+        
+        @param entries list of entries to be shown (list of strings)
+        @param parent reference to the parent widget (QWidget)
+        """
+        super().__init__(parent)
+        self.setupUi(self)
+        
+        self.purgeList.addItems(sorted(entries))
