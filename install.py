d733fb0b376f605edd7d3e8119116eaf69c2c27b
Detlev Offenbach <detlev@die-offenbachs.de>
2012-07-29 18:12:50 +0200

    Fixed a PEP-8 related issue.

--- install.py
+++ install.py
@@ -957,7 +957,7 @@ def main(argv):
     progName = os.path.basename(argv[0])
     
     if os.path.dirname(argv[0]):
-      os.chdir(os.path.dirname(argv[0]))
+        os.chdir(os.path.dirname(argv[0]))
 
     initGlobals()
 
