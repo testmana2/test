9672a61d0591c04ba3600444e04079393afcc701
Detlev Offenbach <detlev@die-offenbachs.de>
2012-11-01 18:50:50 +0100

    Corrected a typo in the upper most documentation file.

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@
 #
 
 """
-Package implementing the eric5 Python IDE (version 5.2).
+Package implementing the eric5 Python IDE (version 5.3).
 
 To get more information about eric5 please see the
 <a href="http://eric-ide.python-projects.org/index.html">eric web site</a>.
