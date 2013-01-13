e9c52df35c73e73ae5dbe1e166146eca298d3064
Detlev Offenbach <detlev@die-offenbachs.de>
2013-01-13 12:58:08 +0100

    Fixed an issue with the IRC widget.

---
+++ Helpviewer/Bookmarks/BookmarkPropertiesDialog.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2012 - 2013 Detlev Offenbach <detlev@die-offenbachs.de>
+#
+
+"""
+Module implementing a dialog to show and edit bookmark properties.
+"""
+
+from PyQt4.QtGui import QDialog
+
+from .Ui_BookmarkPropertiesDialog import Ui_BookmarkPropertiesDialog
+
+from .BookmarkNode import BookmarkNode
+
+import Helpviewer.HelpWindow
+
+
+class BookmarkPropertiesDialog(QDialog, Ui_BookmarkPropertiesDialog):
+    """
+    Class implementing a dialog to show and edit bookmark properties.
+    """
+    def __init__(self, node, parent=None):
+        """
+        Constructor
+        
+        @param parent reference to the parent widget (QWidget)
+        """
+        super().__init__(parent)
+        self.setupUi(self)
+        
+        self.__node = node
+        if self.__node.type() == BookmarkNode.Folder:
+            self.addressLabel.hide()
+            self.addressEdit.hide()
+        
+        self.nameEdit.setText(self.__node.title)
+        self.descriptionEdit.setPlainText(self.__node.desc)
+        self.addressEdit.setText(self.__node.url)
+    
+    def accept(self):
+        """
+        Public slot handling the acceptance of the dialog.
+        """
+        if (self.__node.type() == BookmarkNode.Bookmark and \
+            not self.addressEdit.text()) or \
+           not self.nameEdit.text():
+            super().accept()
+            return
+        
+        bookmarksManager = Helpviewer.HelpWindow.HelpWindow.bookmarksManager()
+        title = self.nameEdit.text()
+        if title != self.__node.title:
+            bookmarksManager.setTitle(self.__node, title)
+        if self.__node.type() == BookmarkNode.Bookmark:
+            url = self.addressEdit.text()
+            if url != self.__node.url:
+                bookmarksManager.setUrl(self.__node, url)
+        description = self.descriptionEdit.toPlainText()
+        if description != self.__node.desc:
+            self.__node.desc = description
+            bookmarksManager.setNodeChanged(self.__node)
+        
+        super().accept()
---
+++ QScintilla/Lexers/LexerJava.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2003 - 2013 Detlev Offenbach <detlev@die-offenbachs.de>
+#
+
+"""
+Module implementing a Java lexer with some additional methods.
+"""
+
+from PyQt4.Qsci import QsciLexerJava,  QsciScintilla
+
+from .Lexer import Lexer
+import Preferences
+
+
+class LexerJava(QsciLexerJava, Lexer):
+    """
+    Subclass to implement some additional lexer dependant methods.
+    """
+    def __init__(self, parent=None):
+        """
+        Constructor
+        
+        @param parent parent widget of this lexer
+        """
+        super().__init__(parent)
+        Lexer.__init__(self)
+        
+        self.commentString = "//"
+        self.streamCommentString = {
+            'start': '/* ',
+            'end': ' */'
+        }
+        self.boxCommentString = {
+            'start': '/* ',
+            'middle': ' * ',
+            'end': ' */'
+        }
+
+    def initProperties(self):
+        """
+        Public slot to initialize the properties.
+        """
+        self.setFoldComments(Preferences.getEditor("CppFoldComment"))
+        self.setFoldPreprocessor(Preferences.getEditor("CppFoldPreprocessor"))
+        self.setFoldAtElse(Preferences.getEditor("CppFoldAtElse"))
+        indentStyle = 0
+        if Preferences.getEditor("CppIndentOpeningBrace"):
+            indentStyle |= QsciScintilla.AiOpening
+        if Preferences.getEditor("CppIndentClosingBrace"):
+            indentStyle |= QsciScintilla.AiClosing
+        self.setAutoIndentStyle(indentStyle)
+        self.setFoldCompact(Preferences.getEditor("AllFoldCompact"))
+    
+    def isCommentStyle(self, style):
+        """
+        Public method to check, if a style is a comment style.
+        
+        @return flag indicating a comment style (boolean)
+        """
+        return style in [QsciLexerJava.Comment,
+                         QsciLexerJava.CommentDoc,
+                         QsciLexerJava.CommentLine,
+                         QsciLexerJava.CommentLineDoc]
+    
+    def isStringStyle(self, style):
+        """
+        Public method to check, if a style is a string style.
+        
+        @return flag indicating a string style (boolean)
+        """
+        return style in [QsciLexerJava.DoubleQuotedString,
+                         QsciLexerJava.SingleQuotedString,
+                         QsciLexerJava.UnclosedString,
+                         QsciLexerJava.VerbatimString]
+    
+    def defaultKeywords(self, kwSet):
+        """
+        Public method to get the default keywords.
+        
+        @param kwSet number of the keyword set (integer)
+        @return string giving the keywords (string) or None
+        """
+        return QsciLexerJava.keywords(self, kwSet)
