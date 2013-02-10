31a8d8dd9fc86896441712c35c54379a7a2d760a
Detlev Offenbach <detlev@die-offenbachs.de>
2013-02-10 14:34:14 +0100

    Fixed an issue in the module parser causing some imports not to be recognised.

---
+++ Helpviewer/Bookmarks/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2009 - 2013 Detlev Offenbach <detlev@die-offenbachs.de>
+#
+
+"""
+Package implementing the bookmarks system.
+"""
---
+++ UtilitiesPython2/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2011 - 2013 Detlev Offenbach <detlev@die-offenbachs.de>
+#
+
+"""
+This package implements some utilities and tools for Python2
+"""
