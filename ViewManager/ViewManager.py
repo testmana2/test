9c4b64e679f844e41e4c09f0553064e116bf2e25
Detlev Offenbach <detlev@die-offenbachs.de>
2013-03-11 19:00:54 +0100

    Made the activation state of the "Save All" action dependent upon the overall modification status of all open editors.

--- ViewManager/ViewManager.py
+++ ViewManager/ViewManager.py
@@ -5734,6 +5734,12 @@ class ViewManager(QObject):
             
             self.checkActions.emit(editor)
         
+        saveAllEnable = False
+        for editor in self.editors:
+            if editor.isModified():
+                saveAllEnable = True
+        self.saveAllAct.setEnabled(saveAllEnable)
+        
     def preferencesChanged(self):
         """
         Public slot to handle the preferencesChanged signal.
