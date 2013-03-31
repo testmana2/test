674ef0fce6c0c3d10376b881e16fafcfca97b89b
Jaime Seuma <jaims.seuma@gmail.com>
2013-03-31 13:04:10 +0200

    I18n update for Spanish, default branch (31st March 2013).

---
+++ Helpviewer/OfflineStorage/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2010 - 2013 Detlev Offenbach <detlev@die-offenbachs.de>
+#
+
+"""
+Package implementing the offline storage related dialogs and models.
+"""
