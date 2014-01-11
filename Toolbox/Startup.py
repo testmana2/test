54515b0bbe1384a5b556225c9189d91f47fb0d66
Detlev Offenbach <detlev@die-offenbachs.de>
2014-01-11 13:05:23 +0100

    Made a change to the tr() change to handle using eric5 with Qt5.

--- Toolbox/Startup.py
+++ Toolbox/Startup.py
@@ -10,7 +10,8 @@ Module implementing some startup helper funcions.
 import os
 import sys
 
-from PyQt4.QtCore import QTranslator, QLocale, QLibraryInfo, QDir, QTextCodec
+from PyQt4.QtCore import QTranslator, QLocale, QLibraryInfo, QDir, \
+    QTextCodec, qVersion
 from PyQt4.QtGui import QApplication
 
 from E5Gui.E5Application import E5Application
@@ -161,8 +162,9 @@ def loadTranslators(qtTransDir, app, translationFiles=()):
     
     global loaded_translators
     
-    # set the default encoding for tr()
-    QTextCodec.setCodecForTr(QTextCodec.codecForName("utf-8"))
+    if qVersion() < "5.0.0":
+        # set the default encoding for tr()
+        QTextCodec.setCodecForTr(QTextCodec.codecForName("utf-8"))
     
     translations = ("qt", "eric5") + translationFiles
     loc = Preferences.getUILanguage()
