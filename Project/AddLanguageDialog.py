99b44541db865e97d618d55fa2d1d3c1d9f57818
Jaime Seuma <jaims.seuma@gmail.com>
2014-02-17 19:00:36 +0100

    eric5 i18n-es update default branch

---
+++ Project/AddLanguageDialog.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2002 - 2014 Detlev Offenbach <detlev@die-offenbachs.de>
+#
+
+"""
+Module implementing a dialog to add a new language to the project.
+"""
+
+from PyQt4.QtGui import QDialog
+
+from .Ui_AddLanguageDialog import Ui_AddLanguageDialog
+
+
+class AddLanguageDialog(QDialog, Ui_AddLanguageDialog):
+    """
+    Class implementing a dialog to add a new language to the project.
+    """
+    def __init__(self, parent=None, name=None):
+        """
+        Constructor
+        
+        @param parent parent widget of this dialog (QWidget)
+        @param name name of this dialog (string)
+        """
+        super().__init__(parent)
+        if name:
+            self.setObjectName(name)
+        self.setupUi(self)
+        
+    def getSelectedLanguage(self):
+        """
+        Public method to retrieve the selected language.
+        
+        @return the selected language (string)
+        """
+        return self.languageCombo.currentText()
---
+++ eric5_trpreviewer.pyw
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2011 - 2014 Detlev Offenbach <detlev@die-offenbachs.de>
+#
+
+"""
+Module implementing the Windows entry point.
+"""
+
+from eric5_trpreviewer import main
+
+main()
