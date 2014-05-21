6e2e47b621d3278f862aa862ee4b8ce4b6efdafd
Detlev Offenbach <detlev@die-offenbachs.de>
2014-05-21 18:47:56 +0200

    Fixed an issue in the Subversion vcsMove method.

--- Plugins/VcsPlugins/vcsPySvn/subversion.py
+++ Plugins/VcsPlugins/vcsPySvn/subversion.py
@@ -915,6 +915,8 @@ class Subversion(VersionControl):
             accepted = (dlg.exec_() == QDialog.Accepted)
             if accepted:
                 target, force = dlg.getData()
+            if not target:
+                return False
         
         if not rx_prot.exactMatch(target):
             isDir = os.path.isdir(name)
