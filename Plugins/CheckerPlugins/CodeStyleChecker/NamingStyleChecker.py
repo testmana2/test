955efd0b2bfc44353cbef2e5b0352bf57054c5f1
Detlev Offenbach <detlev@die-offenbachs.de>
2014-06-21 18:11:38 +0200

    Fixed an issue in the naming style checker caused by AST changes in Python 3.4.

--- Plugins/CheckerPlugins/CodeStyleChecker/NamingStyleChecker.py
+++ Plugins/CheckerPlugins/CodeStyleChecker/NamingStyleChecker.py
@@ -11,6 +11,7 @@ import collections
 import ast
 import re
 import os
+import sys
 
 from PyQt4.QtCore import QT_TRANSLATE_NOOP, QCoreApplication
 
@@ -368,12 +369,20 @@ class NamingStyleChecker(object):
             (integer, integer, string)
         """
         if node.args.kwarg is not None:
-            if not self.LowercaseRegex.match(node.args.kwarg):
+            if sys.version_info >= (3, 4):
+                kwarg = node.args.kwarg.arg
+            else:
+                kwarg = node.args.kwarg
+            if not self.LowercaseRegex.match(kwarg):
                 yield self.__error(node, "N803")
                 return
         
         if node.args.vararg is not None:
-            if not self.LowercaseRegex.match(node.args.vararg):
+            if sys.version_info >= (3, 4):
+                vararg = node.args.vararg.arg
+            else:
+                vararg = node.args.vararg
+            if not self.LowercaseRegex.match(vararg):
                 yield self.__error(node, "N803")
                 return
         
