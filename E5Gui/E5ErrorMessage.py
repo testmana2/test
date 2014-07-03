ccdafa79020f781470e27d31c511531974dcf1c5
Detlev Offenbach <detlev@die-offenbachs.de>
2014-07-03 19:02:29 +0200

    Fixed a little issue in the error message handler. (grafted from 9f5d0a07e69b47c48d9aa4e309c720165c94d1c0)

--- E5Gui/E5ErrorMessage.py
+++ E5Gui/E5ErrorMessage.py
@@ -109,7 +109,7 @@ def messageHandler(msgType, message):
                 messageType = QCoreApplication.translate(
                     "E5ErrorMessage", "Fatal Error:")
             if isinstance(message, bytes):
-                message = message.decode()
+                message = Utilities.decodeBytes(message)
             message = message.replace("\r\n", "<br/>")\
                              .replace("\n", "<br/>")\
                              .replace("\r", "<br/>")
