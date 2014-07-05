31bc3707903214302ccfd6beb998c180392ea500
Detlev Offenbach <detlev@die-offenbachs.de>
2014-07-05 11:41:14 +0200

    Continued renaming eric5 for PyQt5 to eric6.

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@
 #
 
 """
-Package implementing the eric5 Python IDE (version 5.5).
+Package implementing the eric6 Python IDE (version 6.0).
 
 To get more information about eric5 please see the
 <a href="http://eric-ide.python-projects.org/index.html">eric web site</a>.
