dba1c46631c7fdb07e6f9a6ce2ab1b115ee65dc8
Detlev Offenbach <detlev@die-offenbachs.de>
2014-10-19 11:39:57 +0200

    Removed obselete code in hg.py. (grafted from 52e44def50d079b06bc96edf585f1d005005eec6)

--- Plugins/VcsPlugins/vcsMercurial/hg.py
+++ Plugins/VcsPlugins/vcsMercurial/hg.py
@@ -2013,7 +2013,6 @@ class Hg(VersionControl):
         res = dia.startProcess(args, repodir)
         if res:
             dia.exec_()
-            res = dia.hasAddOrDelete()
         self.checkVCSStatus()
     
     def hgInfo(self, ppath, mode="heads"):
