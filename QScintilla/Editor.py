fc2bf77f5158d927bc1992772335e014e68e0c57
Detlev Offenbach <detlev@die-offenbachs.de>
2015-08-19 11:00:48 +0200

    Little enhancement to the editor context menu to enable the Tools submenu only, if it contains some entries (populated by plug-ins). (grafted from 7847a37ee71db8d14ae6aa208c4c521f450c6942)

--- QScintilla/Editor.py
+++ QScintilla/Editor.py
@@ -4766,6 +4766,8 @@ class Editor(QsciScintillaCompat):
             self.menuActs["NewSplit"].setIcon(
                 UI.PixmapCache.getIcon("splitVertical.png"))
         
+        self.menuActs["Tools"].setEnabled(not self.toolsMenu.isEmpty())
+        
         self.showMenu.emit("Main", self.menu, self)
         
     def __showContextMenuAutocompletion(self):
